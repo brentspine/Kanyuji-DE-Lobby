92dd274d7e11d02af2acb126bd9812b7692c99eb
Lukas <lukasdev.private@gmail.com>
2021-10-27 20:50:48 +0200

    PLayerConnectionListener bearbeitet

---
+++ src/main/java/de/kanyuji/lobby/listeners/PLayerConnectionListener.java
@@ -0,0 +1,28 @@
+package de.kanyuji.lobby.listeners;
+
+import de.kanyuji.lobby.Main;
+import de.kanyuji.lobby.utils.ItemBuilder;
+import de.kanyuji.lobby.utils.LocationUtil;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class PLayerConnectionListener implements Listener {
+
+    @EventHandler
+    public void handlePlayerJoinEvent(PlayerJoinEvent event) {
+        event.setJoinMessage(null);
+        Player player = event.getPlayer();
+        player.getInventory().clear();
+        player.teleport(new LocationUtil(Main.getInstance(), "loc.spawn").getLocation());
+        player.getInventory().setItem(0, new ItemBuilder(Material.FIREWORK_ROCKET).setDisplayName("§b§lTeleporter").build());
+        player.getInventory().setItem(1, new ItemBuilder(Material.RED_DYE).setDisplayName("§c§lSpieler Versteckt").build());
+        player.getInventory().setItem(4, new ItemBuilder(Material.LEGACY_FIREWORK_CHARGE).setDisplayName("§7Gadget").build());
+        player.getInventory().setItem(7, new ItemBuilder(Material.CHEST).setDisplayName("§6§lInventar").build());
+        player.getInventory().setItem(8, new ItemBuilder(Material.LEGACY_SKULL).setSkullOwner(player.getName()).setDisplayName("§6§lInventar").build());
+
+    }
+}
