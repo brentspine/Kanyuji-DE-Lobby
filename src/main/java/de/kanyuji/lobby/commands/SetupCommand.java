0d135358024b151f07fc14672ac755e16307c88a
youbo <stolze.luca@gmail.com>
2021-10-27 20:47:55 +0200

    Spawnlocation in Main und SetupCommand

--- src/main/java/de/kanyuji/lobby/commands/SetupCommand.java
+++ src/main/java/de/kanyuji/lobby/commands/SetupCommand.java
@@ -1,6 +1,7 @@
 package de.kanyuji.lobby.commands;
 
 import de.kanyuji.lobby.Main;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,12 +9,28 @@ import org.bukkit.entity.Player;
 
 public class SetupCommand implements CommandExecutor {
 
+    // /setup [spawn]
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(!(sender instanceof Player)) {
             sender.sendMessage(Main.PREFIX + "Dieser Befehl ist nur für InGame");
+            return true;
         }
-        return false;
+        Player player = (Player) sender;
+        if(args.length <= 0) {
+            player.sendMessage(Main.PREFIX + "Verwendung: /" + label + " [spawn]");
+            return true;
+        }
+        if (args[0].equalsIgnoreCase("spawn")) {
+            if(player.getLocation().subtract(0, 1, 0).getBlock().getType() == Material.AIR) {
+                player.sendMessage(Main.PREFIX + "Der Spawn darf nicht in der Luft liegen!");
+                return true;
+            }
+            Main.setSpawnLocation(player.getLocation());
+            return true;
+        }
+        return true;
     }
 
     //todo setup command
