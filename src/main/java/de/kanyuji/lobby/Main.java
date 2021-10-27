e30ea64e3783fcb007085e8a20b2eac17ad31d54
youbo <stolze.luca@gmail.com>
2021-10-27 20:08:07 +0200

    Basic Main Structure und getInstance

--- src/main/java/de/kanyuji/lobby/Main.java
+++ src/main/java/de/kanyuji/lobby/Main.java
@@ -1,8 +1,27 @@
 package de.kanyuji.lobby;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
+    private static Main instance;
+    public static final String PREFIX = "§b§lLobby §8» §7";
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        instance = this;
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
 
     //todo
     //mach halt das
