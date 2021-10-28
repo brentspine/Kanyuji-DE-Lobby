13bc894809887bf79435fb4f2165feb8a3902ba5
youbo <stolze.luca@gmail.com>
2021-10-28 20:17:59 +0200

    PlayerHider Fix

--- src/main/java/de/kanyuji/lobby/listeners/BlockedListeners.java
+++ src/main/java/de/kanyuji/lobby/listeners/BlockedListeners.java
@@ -12,6 +12,7 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -59,6 +60,13 @@ public class BlockedListeners implements Listener {
             if(event.getRawSlot() <= 8) {
                 event.setCancelled(true);
             }
+        }
+    }
+
+    @EventHandler
+    public void onItemDrop(PlayerDropItemEvent event) {
+        if(event.getPlayer().getGameMode() != GameMode.CREATIVE) {
+            event.setCancelled(true);
         }
     }
 
--- src/main/java/de/kanyuji/lobby/listeners/HideListener.java
+++ src/main/java/de/kanyuji/lobby/listeners/HideListener.java
@@ -21,9 +21,9 @@ public class HideListener implements Listener {
             if(event.getItem().getType() == Material.LIME_DYE) {
                 player.getInventory().setItem(1, new ItemBuilder(Material.PURPLE_DYE).setDisplayName("§9Spieler §8» §7Nur VIP").build());
                 player.sendMessage(Main.PREFIX + "Du siehst nun jeden VIP");
-                for(Player hided : Bukkit.getOnlinePlayers()) {
-                    if(!hided.hasPermission("system.vip")) {
-                        player.hidePlayer(hided);
+                for(Player hidden : Bukkit.getOnlinePlayers()) {
+                    if(!hidden.hasPermission("system.vip")) {
+                        player.hidePlayer(hidden);
                     }
                 }
                 if(!Main.getInstance().getVisibleHandler().getHider().contains(player)) {
