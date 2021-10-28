8c1b87fcb8de2be89f5ee4939542b452e03611d0
Lukas <lukasdev.private@gmail.com>
2021-10-28 20:48:10 +0200

    Cosmetical Change

--- src/main/java/de/kanyuji/lobby/listeners/HideListener.java
+++ src/main/java/de/kanyuji/lobby/listeners/HideListener.java
@@ -26,7 +26,7 @@ public class HideListener implements Listener {
             if(!event.getItem().hasItemMeta()) return;
             if(!cooldown.contains(player)) {
                 if(event.getItem().getType() == Material.LIME_DYE) {
-                    player.getInventory().setItem(1, new ItemBuilder(Material.PURPLE_DYE).setDisplayName("§9Spieler §8» §7Nur VIP").build());
+                    player.getInventory().setItem(1, new ItemBuilder(Material.PURPLE_DYE).setDisplayName("§5Spieler §8» §7Nur VIP").build());
                     player.sendMessage(Main.PREFIX + "Du siehst nun jeden VIP");
                     for(Player hided : Bukkit.getOnlinePlayers()) {
                         if(!hided.hasPermission("system.vip")) {
@@ -44,7 +44,7 @@ public class HideListener implements Listener {
                         }
                     }, 60);
                 } else if(event.getItem().getType() == Material.RED_DYE) {
-                    player.getInventory().setItem(1, new ItemBuilder(Material.LIME_DYE).setDisplayName("§9Spieler §8» §7Angezeigt").build());
+                    player.getInventory().setItem(1, new ItemBuilder(Material.LIME_DYE).setDisplayName("§aSpieler §8» §7Angezeigt").build());
                     player.sendMessage(Main.PREFIX + "Du siehst nun jeden Spieler wieder");
                     Bukkit.getOnlinePlayers().forEach(player::showPlayer);
                     if(Main.getInstance().getVisibleHandler().getHider().contains(player)) {
@@ -58,7 +58,7 @@ public class HideListener implements Listener {
                         }
                     }, 60);
                 } else if(event.getItem().getType() == Material.PURPLE_DYE) {
-                    player.getInventory().setItem(1, new ItemBuilder(Material.RED_DYE).setDisplayName("§9Spieler §8» §7Versteckt").build());
+                    player.getInventory().setItem(1, new ItemBuilder(Material.RED_DYE).setDisplayName("§cSpieler §8» §7Versteckt").build());
                     player.sendMessage(Main.PREFIX + "Du siehst nun keinen Spieler Mehr");
                     Bukkit.getOnlinePlayers().forEach( player::hidePlayer);
                     if(!Main.getInstance().getVisibleHandler().getHider().contains(player)) {
