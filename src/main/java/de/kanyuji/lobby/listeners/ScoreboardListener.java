5bed16273812c859a00dec668043591671ddf166
Lukas <lukasdev.private@gmail.com>
2021-10-28 14:27:32 +0200

    Scoreboard angefangen

--- src/main/java/de/kanyuji/lobby/listeners/ScoreboardListener.java
+++ src/main/java/de/kanyuji/lobby/listeners/ScoreboardListener.java
@@ -1,9 +1,39 @@
 package de.kanyuji.lobby.listeners;
 
+import fr.mrmicky.fastboard.FastBoard;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class ScoreboardListener implements Listener {
 
-    //todo Adde scoreboard api in maven
+    public static final Map<UUID, FastBoard> boards = new HashMap<>();
+
+    @EventHandler
+    public void handlePlayerJoinEvent(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        FastBoard board = new FastBoard(player);
+        board.updateTitle("§b§lLobby");
+        boards.put(player.getUniqueId(), board);
+    }
+
+    @EventHandler
+    public void handlePlayerQuitEvent(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+
+        FastBoard board = boards.remove(player.getUniqueId());
+
+        if (board != null) {
+            board.delete();
+        }
+    }
+    //todo update method
+
 
 }
