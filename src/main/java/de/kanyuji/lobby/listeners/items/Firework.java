2e6fa111c22493d742a6721ef1cf35acebb882e7
Lukas <lukasdev.private@gmail.com>
2021-11-14 12:16:15 +0100

    Cosmetical things

--- src/main/java/de/kanyuji/lobby/listeners/items/Firework.java
+++ src/main/java/de/kanyuji/lobby/listeners/items/Firework.java
@@ -81,15 +81,15 @@ public class Firework implements Listener {
     }
 
     public static void setInventory() {
-        inventory.setItem(3, new ItemBuilder(Material.STICK).setDisplayName("§4§lTTT").setLore("§4Spieler §8» §7Todo").build());
-        inventory.setItem(5, new ItemBuilder(Material.WATER_BUCKET).setDisplayName("§3§lSURF").setLore("§4Spieler §8» §7Todo").build());
-        inventory.setItem(10, new ItemBuilder(Material.CRAFTING_TABLE).setDisplayName("§9§lBINGO").setLore("§4Spieler §8» §7Todo").build());
-        inventory.setItem(16, new ItemBuilder(Material.LAVA_BUCKET).setDisplayName("§2§lCHALLENGES").setLore("§4Spieler §8» §7Todo").build());
-        inventory.setItem(22, new ItemBuilder(Material.FIREWORK_ROCKET).setDisplayName("§e§lEvent").build());
+        inventory.setItem(3, new ItemBuilder(Material.STICK).setDisplayName("§4§lTTT").setLore("§4TTT §7ist ein Rollenspiel, wo es Mörder, Dedektive und Unschuldige gibt.", "§7Die Mörder müssen getötet werden.").build());
+        inventory.setItem(5, new ItemBuilder(Material.WATER_BUCKET).setDisplayName("§3§lSURF").setLore("§3Surf §7ist ein FFA Modus.", "§7Wasser boostet dich jedoch hoch!").build());
+        inventory.setItem(10, new ItemBuilder(Material.CRAFTING_TABLE).setDisplayName("§9§lBINGO").setLore("§9Bingo §7ist ein Modus, in dem du eine Liste an Items finden musst, um zu gewinnen").build());
+        inventory.setItem(16, new ItemBuilder(Material.LAVA_BUCKET).setDisplayName("§2§lCHALLENGES").setLore("§2Challanges §7ist ein Modus, wo du alleine oder mit deinen Freunden herausforderungen abschliesen kannst!").build());
+        inventory.setItem(22, new ItemBuilder(Material.FIREWORK_ROCKET).setDisplayName("§e§lEvent").setLore("§eEvent §7Ist der Eventplatz. Dort finden manchmal Events stadt").build());
         inventory.setItem(31, new ItemBuilder(Material.FIRE_CHARGE).setDisplayName("§7Spawn").build());
-        inventory.setItem(37, new ItemBuilder(Material.CLAY_BALL).setDisplayName("§c§lSWITCH").build());
-        inventory.setItem(43, new ItemBuilder(Material.DIAMOND_BOOTS).setDisplayName("§b§lTRYJUMP").build());
-        inventory.setItem(48, new ItemBuilder(Material.ELYTRA).setDisplayName("§a§lELYTRAWARS").build());
+        inventory.setItem(37, new ItemBuilder(Material.CLAY_BALL).setDisplayName("§c§lSWITCH").setLore("§cSwitch §7ist ein Modus, in dem mehrere FFA Modis immer switchen").build());
+        inventory.setItem(43, new ItemBuilder(Material.DIAMOND_BOOTS).setDisplayName("§b§lTRYJUMP").setLore("bTryJump §7ist ein Modus, in dem du Punkte durch JnR verdienen musst und am ende etwas damit kaufst.", "§7Mit den gekauften items wird PVP gespielt").build());
+        inventory.setItem(48, new ItemBuilder(Material.ELYTRA).setDisplayName("§a§lELYTRAWARS").setLore("§aElytrawars §7ist ein Modus, in dem du während dem Elytraflug kämpfst").build());
         inventory.setItem(50, new ItemBuilder(Material.BARRIER).setDisplayName("§9Coming Soon").build());
     }
 }
